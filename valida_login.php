4ec933a4cf3ad74081eea8584498b29f06c0658c
VictorMont <victormonteir2@gmail.com>
2021-05-23 19:20:58 -0300

    Tela de login e autenticação - First Commit"

---
+++ valida_login.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+
+    $usuarios = [
+        ['email' => 'luffy@gmail', 'senha' => 'carne'],
+        ['email' => 'zoro@gmail', 'senha' => 'espada'],
+        ['email' => 'nami@gmail', 'senha' => 'dinheiro'],
+        ['email' => 'robin@gmail', 'senha' => 'historia']
+    ];
+    /*echo '<pre>';
+    print_r($usuarios);
+    echo '</pre>';*/
+    ///print_r($_POST);
+    $usuario_autenticado = false;
+    
+    foreach ($usuarios as $user){
+        /*echo 'Usuario: ' . $user['email'] . ' / '. $user['senha'];
+        echo '<br> Digitado: ';
+        print_r($_POST);
+        echo '<hr>';*/
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticado = true;
+        }
+    }
+    
+    if($usuario_autenticado){
+        header('Location: home.php');
+        $_SESSION['autenticado'] = 'YES';
+    }else{
+        header('Location: index.php?login=erro');//passando pela variavel login o valor de erro
+        $_SESSION['autenticado'] = 'NO';
+    }
+
+
+
+?>
